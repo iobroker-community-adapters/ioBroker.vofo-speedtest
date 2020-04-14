fa9ef7b4d1133dfdf1e398ade908d615634a0c6f
Peter Baumert <peter.baumert@outlook.com>
2020-04-14 19:13:06 +0200

    initial

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"vodafone-speedtest adapter settings": {
+		"en": "Adapter settings for vodafone-speedtest",
+		"de": "Adaptereinstellungen für vodafone-speedtest",
+		"ru": "Настройки адаптера для vodafone-speedtest",
+		"pt": "Configurações do adaptador para vodafone-speedtest",
+		"nl": "Adapterinstellingen voor vodafone-speedtest",
+		"fr": "Paramètres d'adaptateur pour vodafone-speedtest",
+		"it": "Impostazioni dell'adattatore per vodafone-speedtest",
+		"es": "Ajustes del adaptador para vodafone-speedtest",
+		"pl": "Ustawienia adaptera dla vodafone-speedtest",
+		"zh-cn": "vodafone-speedtest的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
